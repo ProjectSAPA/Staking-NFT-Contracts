919e8262f85e6d5ff2a320b470e9655b99736edd
Hentai Ousama <kutabare.eerito@gmail.com>
2022-04-19 18:16:43 +0530

    Update Staking NFT.sol

--- Staking NFT.sol
+++ Staking NFT.sol
@@ -64,23 +64,181 @@ library StringTools {
     }
 }
 
+library Base64 {
+    string internal constant TABLE_ENCODE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+    bytes  internal constant TABLE_DECODE = hex"0000000000000000000000000000000000000000000000000000000000000000"
+                                            hex"00000000000000000000003e0000003f3435363738393a3b3c3d000000000000"
+                                            hex"00000102030405060708090a0b0c0d0e0f101112131415161718190000000000"
+                                            hex"001a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132330000000000";
+
+    function encode(bytes memory data) internal pure returns (string memory) {
+        if (data.length == 0) return '';
+
+        // load the table into memory
+        string memory table = TABLE_ENCODE;
+
+        // multiply by 4/3 rounded up
+        uint256 encodedLen = 4 * ((data.length + 2) / 3);
+
+        // add some extra buffer at the end required for the writing
+        string memory result = new string(encodedLen + 32);
+
+        assembly {
+            // set the actual output length
+            mstore(result, encodedLen)
+
+            // prepare the lookup table
+            let tablePtr := add(table, 1)
+
+            // input ptr
+            let dataPtr := data
+            let endPtr := add(dataPtr, mload(data))
+
+            // result ptr, jump over length
+            let resultPtr := add(result, 32)
+
+            // run over the input, 3 bytes at a time
+            for {} lt(dataPtr, endPtr) {}
+            {
+                // read 3 bytes
+                dataPtr := add(dataPtr, 3)
+                let input := mload(dataPtr)
+
+                // write 4 characters
+                mstore8(resultPtr, mload(add(tablePtr, and(shr(18, input), 0x3F))))
+                resultPtr := add(resultPtr, 1)
+                mstore8(resultPtr, mload(add(tablePtr, and(shr(12, input), 0x3F))))
+                resultPtr := add(resultPtr, 1)
+                mstore8(resultPtr, mload(add(tablePtr, and(shr( 6, input), 0x3F))))
+                resultPtr := add(resultPtr, 1)
+                mstore8(resultPtr, mload(add(tablePtr, and(        input,  0x3F))))
+                resultPtr := add(resultPtr, 1)
+            }
+
+            // padding with '='
+            switch mod(mload(data), 3)
+            case 1 { mstore(sub(resultPtr, 2), shl(240, 0x3d3d)) }
+            case 2 { mstore(sub(resultPtr, 1), shl(248, 0x3d)) }
+        }
+
+        return result;
+    }
+
+    function decode(string memory _data) internal pure returns (bytes memory) {
+        bytes memory data = bytes(_data);
+
+        if (data.length == 0) return new bytes(0);
+        require(data.length % 4 == 0, "invalid base64 decoder input");
+
+        // load the table into memory
+        bytes memory table = TABLE_DECODE;
+
+        // every 4 characters represent 3 bytes
+        uint256 decodedLen = (data.length / 4) * 3;
+
+        // add some extra buffer at the end required for the writing
+        bytes memory result = new bytes(decodedLen + 32);
+
+        assembly {
+            // padding with '='
+            let lastBytes := mload(add(data, mload(data)))
+            if eq(and(lastBytes, 0xFF), 0x3d) {
+                decodedLen := sub(decodedLen, 1)
+                if eq(and(lastBytes, 0xFFFF), 0x3d3d) {
+                    decodedLen := sub(decodedLen, 1)
+                }
+            }
+
+            // set the actual output length
+            mstore(result, decodedLen)
+
+            // prepare the lookup table
+            let tablePtr := add(table, 1)
+
+            // input ptr
+            let dataPtr := data
+            let endPtr := add(dataPtr, mload(data))
+
+            // result ptr, jump over length
+            let resultPtr := add(result, 32)
+
+            // run over the input, 4 characters at a time
+            for {} lt(dataPtr, endPtr) {}
+            {
+               // read 4 characters
+               dataPtr := add(dataPtr, 4)
+               let input := mload(dataPtr)
+
+               // write 3 bytes
+               let output := add(
+                   add(
+                       shl(18, and(mload(add(tablePtr, and(shr(24, input), 0xFF))), 0xFF)),
+                       shl(12, and(mload(add(tablePtr, and(shr(16, input), 0xFF))), 0xFF))),
+                   add(
+                       shl( 6, and(mload(add(tablePtr, and(shr( 8, input), 0xFF))), 0xFF)),
+                               and(mload(add(tablePtr, and(        input , 0xFF))), 0xFF)
+                    )
+                )
+                mstore(resultPtr, shl(232, output))
+                resultPtr := add(resultPtr, 3)
+            }
+        }
+
+        return result;
+    }
+}
+
+
+interface IUriGenerator {
+    function operator() external view returns(string memory);
+
+    function getEncodedSvg(
+        string memory stakeTokenSymbol,
+        string memory rewardTokenSymbol,
+        string memory stakedAmount,
+        string memory stakeShare,
+        string memory poolIndex
+    ) external view returns(string memory);
+
+    function getJSON(
+        string memory name,
+        string memory stakeToken,
+        string memory rewardToken,
+        string memory poolIndex,
+        string memory encodedSvg,
+        string memory stakedAmount,
+        string memory availableRewards,
+        string memory withdrawnRewards
+    ) external pure returns(string memory);
+
+    function getTokenURI(
+        string memory json
+    ) external pure returns(string memory);
+}
 
 interface IERC20 {
-    function totalSupply() external view returns (uint256);
+    function name() external view returns (string memory);
+    function symbol() external view returns (string memory);
+    function decimals() external view returns (uint8);
 
-    function balanceOf(address account) external view returns (uint256);
+    function totalSupply() external view returns (uint);
+    function balanceOf(address account) external view returns (uint);
+    function transfer(address recipient, uint amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint);
+    function approve(address spender, uint amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint amount) external returns (bool);
 
-    function transfer(address recipient, uint256 amount) external returns (bool);
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
+}
 
-    function allowance(address owner, address spender) external view returns (uint256);
-
-    function approve(address spender, uint256 amount) external returns (bool);
-
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-
-    event Transfer(address indexed from, address indexed to, uint256 value);
-
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+interface StakingContract {
+    function getNFTAttributes(
+        IERC20 stakeToken,
+        IERC20 rewardToken,
+        uint256 poolIndex,
+        uint256 tokenId
+    ) external view returns (string memory, string memory, string memory, string memory);
 }
 
 
@@ -359,7 +517,8 @@ contract NFT_ERC_721 is NFToken, Ownable {
 
     uint256 nextTokenId = 1;
     address public minter;
-    string public imageUrl;
+    StakingContract public stakingContract;
+    IUriGenerator public uriGenerator = IUriGenerator(address(0));
 
     struct PoolIdentifier {
         IERC20 stakeToken;
@@ -373,12 +532,16 @@ contract NFT_ERC_721 is NFToken, Ownable {
     mapping(uint256 => PoolIdentifier) public tokenIdToPoolIdentifier;
     mapping(IERC20 => mapping(IERC20 => mapping(uint256 => mapping(address => uint256[])))) public poolTypeAndOwnerToTokenId;
 
-    constructor(string memory name, string memory symbol, address _minter, string memory _imageUrl) {
+    constructor(string memory name, string memory symbol, address _minter) {
         supportedInterfaces[0x5b5e139f] = true;
         nftName = name;
         nftSymbol = symbol;
         minter = _minter;
-        imageUrl = _imageUrl;
+        stakingContract = StakingContract(_minter);
+    }
+
+    function setUriGenerator(IUriGenerator _uriGenerator) external onlyOwner() {
+        uriGenerator = _uriGenerator;
     }
 
     modifier onlyMinter() {
@@ -435,6 +598,7 @@ contract NFT_ERC_721 is NFToken, Ownable {
         IERC20 rewardToken,
         uint256 poolIndex
     ) external onlyMinter() returns (uint256 tokenId) {
+        require(address(uriGenerator) != address(0), "Uri Generator has not been set yet.");
         _mint(_to, nextTokenId);
 
         PoolIdentifier storage poolIdentifier = tokenIdToPoolIdentifier[nextTokenId];
@@ -478,13 +642,27 @@ contract NFT_ERC_721 is NFToken, Ownable {
         nftSymbol = symbol;
     }
 
-    function setImageUrl(string memory _uri) external onlyOwner() {
-        imageUrl = _uri;
+    function getTokenParameters(uint256 _tokenId) external view validNFToken(_tokenId) returns(IERC20, IERC20, uint256) {
+        PoolIdentifier storage poolIdentifier = tokenIdToPoolIdentifier[_tokenId];
+        return (poolIdentifier.stakeToken, poolIdentifier.rewardToken, poolIdentifier.poolIndex);
     }
 
-    function getDetailsOfToken(uint256 _tokenId) internal view returns (string memory, string memory, string memory) {
-        PoolIdentifier storage poolIdentifier = tokenIdToPoolIdentifier[_tokenId];
+    function getEncodedSvg(
+        PoolIdentifier storage poolIdentifier,
+        string memory poolIndex,
+        string memory stakedAmount,
+        string memory stakeShare
+    ) internal view returns (string memory) {
+        return uriGenerator.getEncodedSvg(
+            poolIdentifier.stakeToken.symbol(),
+            poolIdentifier.rewardToken.symbol(),
+            stakedAmount,
+            stakeShare,
+            poolIndex
+        );
+    }
 
+    function getDetailsOfToken(PoolIdentifier storage poolIdentifier) internal view returns (string memory, string memory, string memory) {
         return (
 			address(poolIdentifier.stakeToken).toString(),
 			address(poolIdentifier.rewardToken).toString(),
@@ -492,35 +670,44 @@ contract NFT_ERC_721 is NFToken, Ownable {
         );
     }
 
-    function getTokenParameters(uint256 _tokenId) external view validNFToken(_tokenId) returns(IERC20, IERC20, uint256) {
-        PoolIdentifier storage poolIdentifier = tokenIdToPoolIdentifier[_tokenId];
-        return (poolIdentifier.stakeToken, poolIdentifier.rewardToken, poolIdentifier.poolIndex);
+    function getTokenURI(
+        PoolIdentifier storage poolIdentifier,
+        string memory stakeToken,
+        string memory rewardToken,
+        string memory poolIndex,
+        string memory stakedAmount,
+        string memory stakeShare,
+        string memory availableRewards,
+        string memory withdrawnRewards
+    ) internal view returns(string memory) {
+        string memory encodedSvg = getEncodedSvg(poolIdentifier, poolIndex, stakedAmount, stakeShare);
+
+        string memory json = uriGenerator.getJSON(
+            nftName,
+            stakeToken,
+            rewardToken,
+            poolIndex,
+            encodedSvg,
+            stakedAmount,
+            availableRewards,
+            withdrawnRewards
+        );
+
+        return uriGenerator.getTokenURI(json);
     }
 
     function tokenURI(uint256 _tokenId) external override view validNFToken(_tokenId) returns (string memory) {
-        (string memory a, string memory b, string memory c) = getDetailsOfToken(_tokenId);
+        PoolIdentifier storage poolIdentifier = tokenIdToPoolIdentifier[_tokenId];
 
-        string memory uri = string(abi.encodePacked(
-            "{"
-                "\"image\": \"", imageUrl, "\", "
-                "\"attributes\": ["
-                    "{"
-                        "\"trait_type\": \"Stake Token\","
-                        "\"value\": \"", a, "\""
-                    "},"
-                    "{"
-                        "\"trait_type\": \"Reward Token\","
-                        "\"value\": \"", b, "\""
-                    "},"
-                    "{"
-                        "\"trait_type\": \"Pool Index\","
-                        "\"value\": \"", c, "\""
-                    "}"
-                "]"
-            "}"
-        ));
+        (string memory stakeToken, string memory rewardToken, string memory poolIndex) = getDetailsOfToken(poolIdentifier);
+        (
+            string memory stakedAmount,
+            string memory stakeShare,
+            string memory availableRewards,
+            string memory withdrawnRewards
+        ) = stakingContract.getNFTAttributes(poolIdentifier.stakeToken, poolIdentifier.rewardToken, poolIdentifier.poolIndex, _tokenId);
 
-        return uri;
+        return getTokenURI(poolIdentifier, stakeToken, rewardToken, poolIndex, stakedAmount, stakeShare, availableRewards, withdrawnRewards);
     }
 
     function getTokenIdsOfOwner(IERC20 stakeToken, IERC20 rewardToken, uint256 poolIndex, address _owner) external view returns (uint256[] memory) {
